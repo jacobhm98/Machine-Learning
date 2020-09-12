441201ff2e12c621c36538fc4f99a20f6dfe2502
Jacob Hedén Malm <jmalm@invidi.com>
2020-09-12 22:22:45 +0200

    drawing my own tree

--- lab1/dectrees/python/lab1.py
+++ lab1/dectrees/python/lab1.py
@@ -1,6 +1,6 @@
 import monkdata as m
 import dtree as d
-
+import numpy as np
 
 def printFirstFourAssignments():
     print("Entropy of the datasets:")
@@ -21,5 +21,34 @@ def printFirstFourAssignments():
             print("Information gain of attribute: " + str(attribute.name))
             print(d.averageGain(dataset, attribute))
 
-def splitOnAttribute():
-    dataset = m.monk1
+def selectAttributeToSplitOn(dataset):
+    index = 0
+    maxGain = 0
+    for i in range(0, len(m.attributes)):
+        if d.averageGain(dataset, m.attributes[i]) > maxGain:
+            index = i
+            maxGain = d.averageGain(dataset, m.attributes[i])
+    return m.attributes[index]
+
+
+def splitDataset(dataset):
+    subsets = []
+    splitAttribute = selectAttributeToSplitOn(dataset)
+    for value in splitAttribute.values:
+        subsets.append(d.select(dataset, splitAttribute, value))
+    return [splitAttribute, subsets]
+
+def createTreeManually():
+    tree = splitDataset(m.monk1)
+    for i in range(0, len(tree[1])):
+        tree[1][i] = splitDataset(tree[1][i])
+    for firstLevelNode in tree[1]:
+        for i in range(0, len(firstLevelNode[1])):
+            firstLevelNode[1][i] = d.mostCommon(firstLevelNode[1][i])
+    return tree
+tree = createTreeManually()
+id1Tree = d.buildTree(m.monk1, m.attributes, 2)
+print("Manually created tree:")
+print(tree)
+print("ID3 created tree 2 levels deep")
+print(id1Tree)
