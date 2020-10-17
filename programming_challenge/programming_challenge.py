094f6bb1f2e02d1fe4710e09e9e1ac39c1bd7553
Jacob Heden Malm <jacmalm@kth.se>
2020-10-18 01:01:42 +0200

    fixed the shit why does .get() return none if doesnt exist stupid language and shitty neural net

--- programming_challenge/programming_challenge.py
+++ programming_challenge/programming_challenge.py
@@ -12,19 +12,17 @@ class NeuralNet(nn.Module):
         super().__init__()
         self.fc1 = nn.Linear(10, 64)
         self.fc2 = nn.Linear(64, 64)
-        self.fc3 = nn.Linear(64, 64)
-        self.fc4 = nn.Linear(64, 3)
+        self.fc3 = nn.Linear(64, 3)
 
     def forward(self, x):
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
-        x = F.relu(self.fc3(x))
-        x = F.log_softmax(self.fc4(x), dim=1)
+        x = F.log_softmax(self.fc3(x), dim=1)
         return x
 
 
 nameToLabel = {"Bob": 0, "Atsuto": 1, "Jörg": 2}
-gradeToFloat = {"A": 0.0, "B": 1.0, "C": 2.0, "D": 3.0, "E": 4.0, "F": 5.0}
+gradeToFloat = {"A": 0.0, "B": 1.0, "C": 2.0, "D": 3.0, "E": 4.0, "F": 5.0, "Fx": 6.0}
 boolToFloat = {"False": 0.0, "True": 1.0}
 
 
@@ -37,14 +35,14 @@ def sanitizeData(X):
     labels = []
     datapoints = []
     for entry in X:
-        labels.append(nameToLabel.get(entry[1]))
+        labels.append(nameToLabel[entry[1]])
         datavector = []
         for i in range(2, Ndims):
             if i == 6:
-                datavector.append(boolToFloat.get(entry[i]))
+                datavector.append(boolToFloat[entry[i]])
                 continue
             if i == 7:
-                datavector.append(gradeToFloat.get(entry[i]))
+                datavector.append(gradeToFloat[entry[i]])
                 continue
             try:
                 datavector.append(float(entry[i]))
@@ -76,16 +74,18 @@ def splitIntoTrainingAndValidation(X, y, valFraction=0.3):
     training_data = torch.Tensor(training_data)
     training_data = F.normalize(training_data)
     training_labels = torch.Tensor(training_labels).long()
+
     test_data = torch.Tensor(test_data)
     test_data = F.normalize(test_data)
     test_labels = torch.Tensor(test_labels).long()
+
     training_set = TensorDataset(training_data, training_labels)
     test_set = TensorDataset(test_data, test_labels)
     return training_set, test_set
 
 def trainNetwork(network, dataloader):
-    learning_rate = 0.001
-    optimizer = torch.optim.SGD(network.parameters(), lr=learning_rate)
+    learning_rate = 0.0001
+    optimizer = torch.optim.Adam(network.parameters(), lr=learning_rate)
     running_loss = 0.0
     for i, datapoint in enumerate(dataloader, 0):
         X, y = datapoint
@@ -95,21 +95,33 @@ def trainNetwork(network, dataloader):
         loss.backward()
         optimizer.step()
         running_loss += loss.item()
-        if i % 20 == 19:
-            print("Iteration: " + str(i))
-            print("loss: " + str(running_loss / 20))
-            running_loss = 0
+    print(running_loss)
+
+def testNetwork(network, dataloader):
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for data in dataloader:
+            X, y = data
+            output = network(X)
+            for idx, i in enumerate(output):
+                if torch.argmax(i) == y[idx]:
+                    correct += 1
+                total += 1
+    print("Accuracy: " + str(correct/total))
+
 
 def main():
     X = importCsv("TrainOnMe.csv")
     X, y = sanitizeData(X)
     X, y = shuffleData(X, y)
-    Z = torch.randn((997, 10)).float()
     training_data, test_data = splitIntoTrainingAndValidation(X, y, 0.3)
     training_loader = DataLoader(training_data, batch_size=1, shuffle=False)
-    test_loader = DataLoader(test_data, batch_size=4, shuffle=False)
+    test_loader = DataLoader(test_data, batch_size=1, shuffle=False)
     net = NeuralNet()
-    EPOCHS = 10
+    EPOCHS = 100
     for i in range(EPOCHS):
         trainNetwork(net, training_loader)
+        testNetwork(net, training_loader)
+        testNetwork(net, test_loader)
 main()
