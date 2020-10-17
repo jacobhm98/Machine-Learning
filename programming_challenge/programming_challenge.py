9dd7354da5787e4a72ab5bd39a9864037dd304b0
Jacob Heden Malm <jacmalm@kth.se>
2020-10-17 19:17:44 +0200

    preprocess the data and build functions for splitting

--- programming_challenge/programming_challenge.py
+++ programming_challenge/programming_challenge.py
@@ -1,4 +1,5 @@
 import numpy as np
+import random
 
 nameToLabel = {"Bob": 0, "Atsuto": 1, "Jörg": 2}
 gradeToFloat = {"A": 0.0, "B": 1.0, "C": 2.0, "D": 3.0, "E": 4.0, "F": 5.0}
@@ -9,13 +10,12 @@ def importCsv(filepath):
     data = np.genfromtxt(filepath, dtype=str, delimiter=',')
     return data
 
-
 def sanitizeData(X):
     Ndims = X.shape[1]
     labels = []
     datapoints = []
     for entry in X:
-        labels.append([int(entry[0]), nameToLabel.get(entry[1])])
+        labels.append(nameToLabel.get(entry[1]))
         datavector = []
         for i in range(2, Ndims):
             if i == 6:
@@ -31,10 +31,33 @@ def sanitizeData(X):
         datapoints.append(datavector)
     return np.array(datapoints), np.array(labels)
 
+def shuffleData(X, y):
+    assert(X.shape[0] == y.shape[0])
+    Npts = X.shape[0]
+    points = []
+    for i in range(Npts):
+       points.append((X[i], y[i]))
+    random.shuffle(points)
+    for i in range(Npts):
+        X[i] = points[i][0]
+        y[i] = points[i][1]
+    return X, y
+
+def splitIntoTrainingAndValidation(X, y, valFraction=0.3):
+    assert X.shape[0] == y.shape[0]
+    Npts = X.shape[0]
+    sliceIndex = int(Npts * valFraction)
+    training_data = X[sliceIndex:]
+    test_data = X[:sliceIndex]
+    training_labels = y[sliceIndex:]
+    test_labels = y[:sliceIndex]
+    return training_data, training_labels, test_data, test_labels
 
 def main():
     X = importCsv("TrainOnMe.csv")
     X, y = sanitizeData(X)
-
-
+    X, y = shuffleData(X, y)
+    X, y, validationX, validationY = splitIntoTrainingAndValidation(X, y, 0.3)
+    print(X.shape[0])
+    print(validationX.shape[0])
 main()
